805bc0430a16dc505e1d6e959663336213bd0545
yinsl <yin_slin@sina.com>
2016-01-11 17:40:59 +0800

    many

---
+++ MyTechTest/src/main/java/com/test/example/apache/kafka/consumer/ConsumerGroupExample.java
@@ -0,0 +1,64 @@
+package com.test.example.apache.kafka.consumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class ConsumerGroupExample {
+	private final Consumer<String, String> consumer;
+	private final static String topic = "topic_test3";
+
+	private final static String groupId = "topic_test3_group";
+
+	public ConsumerGroupExample() {
+		Properties props = new Properties();
+		props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		// "10.25.23.101:9092,10.25.23.102:9092,10.25.23.103:9092");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+		        "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+		        "org.apache.kafka.common.serialization.StringDeserializer");
+		consumer = new KafkaConsumer<String, String>(props);
+
+	}
+
+	public void shutdown() {
+		if (consumer != null) {
+			consumer.close();
+		}
+	}
+
+	public void run() {
+		consumer.subscribe(Arrays.asList(topic));
+		while (true) {
+			System.out.print(".");
+			ConsumerRecords<String, String> records = consumer.poll(100);
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		ConsumerGroupExample example = new ConsumerGroupExample();
+		example.run();
+
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+		example.shutdown();
+	}
+}
---
+++ MyTechTest/src/main/java/com/test/example/apache/kafka/producer/ProducerTest.java
@@ -0,0 +1,65 @@
+package com.test.example.apache.kafka.producer;
+
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class ProducerTest {
+
+	private static final String topic = "my-topic";
+
+//	private static final Logger log = LoggerFactory.getLogger(ProducerTest.class);
+
+	public static void main(String[] args) {
+
+		int events = 5;
+		Random rnd = new Random();
+
+		Properties props = new Properties();
+
+//		props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "10.25.23.101:9092,10.25.23.102:9092,10.25.23.103:9092,10.25.23.104:9092,10.25.23.105:9092,10.25.23.106:9092");
+		props.put(ProducerConfig.ACKS_CONFIG, "all");
+		props.put(ProducerConfig.RETRIES_CONFIG, 0);
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+		        "org.apache.kafka.common.serialization.StringSerializer");
+
+		Producer<String, String> producer = new KafkaProducer<String, String>(props);
+		final CountDownLatch cdl = new CountDownLatch(events);
+		for (long nEvents = 0; nEvents < events; nEvents++) {
+			long runtime = new Date().getTime();
+			String ip = "192.168.2." + rnd.nextInt(255);
+			String msg = runtime + ", www.example.com, " + ip;
+			System.out.println("sending topic: " + topic + ", ip: " + ip + ", msg: " + msg);
+			ProducerRecord<String, String> data = new ProducerRecord<String, String>(topic, ip, msg);
+
+			producer.send(data, new Callback() {
+				@Override
+				public void onCompletion(RecordMetadata metadata, Exception exception) {
+					System.out.println("----sended ---");
+					cdl.countDown();
+				}
+			});
+		}
+		try {
+			cdl.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		producer.close();
+	}
+
+}
