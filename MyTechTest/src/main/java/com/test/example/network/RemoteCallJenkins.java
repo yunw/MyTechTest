eb77ff18b2efbff793b736c55a845e7951093d48
abfme <v_yinshunlin@saicmotor.com>
2016-05-24 21:01:02 +0800

    docker v2 registry

--- MyTechTest/src/main/java/com/test/example/network/RemoteCallJenkins.java
+++ MyTechTest/src/main/java/com/test/example/network/RemoteCallJenkins.java
@@ -1,8 +1,13 @@
 package com.test.example.network;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Properties;
@@ -10,7 +15,10 @@ import java.util.Set;
 
 import org.dom4j.DocumentException;
 
+import com.alibaba.fastjson.JSONObject;
 import com.googlecode.openbox.server.ssh.LinuxClient;
+
+import net.iharder.base64.Base64;
 
 public class RemoteCallJenkins {
 
@@ -25,17 +33,37 @@ public class RemoteCallJenkins {
 		}
 	}
 
-	public static void main(String[] args) throws MalformedURLException, DocumentException {
+	public static void main(String[] args) throws MalformedURLException, DocumentException, InterruptedException {
 //		 remoteDeploy(args);
 		 jobStatus();
 	}
 
 	public static void jobStatus() {
-		LinuxClient client = getLinuxClient();
-		String cmd = props.getProperty("jobStatus");
-		String s = client.executeCommand(cmd);
-		System.out.println(s);
-		client.close();
+		try {
+			URL url = new URL("http://10.25.20.104:8080/job/tracereport/lastBuild/api/json?tree=result");
+			String encoding = Base64.encodeBytes(("abfme:Pass1234").getBytes());
+
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Authorization", "Basic " + encoding);
+			InputStream content = (InputStream) connection.getInputStream();
+			BufferedReader in = new BufferedReader(new InputStreamReader(content));
+			String line;
+			StringBuilder sb = new StringBuilder();
+			while ((line = in.readLine()) != null) {
+				sb.append(line);
+			}
+			System.out.println(sb.toString());
+			JSONObject jsonObj = JSONObject.parseObject(sb.toString());
+			System.out.println(jsonObj.toJSONString());
+			System.out.println(jsonObj.getString("result"));
+			in.close();
+			content.close();
+			connection.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@SuppressWarnings("unchecked")
---
+++ MyTechTest/src/main/java/com/test/example/network/jenkins/model/JenkinsResult.java
@@ -0,0 +1,75 @@
+package com.test.example.network.jenkins.model;
+
+import java.util.List;
+
+public class JenkinsResult {
+	
+	private List<Action> actions;
+	
+	class Action {
+		
+		class BuildsByBranchName {
+			
+		}
+		
+		class Cause {
+			
+			private String shortDescription;
+			
+			private String userId;
+			
+			private String userName;
+
+			public String getShortDescription() {
+				return shortDescription;
+			}
+
+			public void setShortDescription(String shortDescription) {
+				this.shortDescription = shortDescription;
+			}
+
+			public String getUserId() {
+				return userId;
+			}
+
+			public void setUserId(String userId) {
+				this.userId = userId;
+			}
+
+			public String getUserName() {
+				return userName;
+			}
+
+			public void setUserName(String userName) {
+				this.userName = userName;
+			}
+			
+		}
+		
+		class Parameter {
+			
+			private String name;
+			
+			private String value;
+
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public String getValue() {
+				return value;
+			}
+
+			public void setValue(String value) {
+				this.value = value;
+			}
+			
+		}
+
+	}
+
+}
